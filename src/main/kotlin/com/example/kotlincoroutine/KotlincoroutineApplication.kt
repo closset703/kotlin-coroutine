14364d0b804f5437053b0d3022122f967b46cefe
이필수 <closset703@nate.com>
2020-08-09 21:43:20 +0900

    프로젝트 초기화

---
+++ src/main/kotlin/com/example/kotlincoroutine/KotlincoroutineApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlincoroutine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlincoroutineApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlincoroutineApplication>(*args)
+}
